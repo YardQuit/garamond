c02250684306c1cc22ebd1dbb9d2878213864a57
Michael A Jones <yardquit@pm.me>
2026-09-21 08:06:08 +0200

    Bump to version 1.0.1

--- garamond.el
+++ garamond.el
@@ -4,7 +4,7 @@
 
 ;; Author: Michael A Jones <yardquit@pm.me>
 ;; Maintainer: Michael A Jones <yardquit@pm.me>
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Package-Requires: ((emacs "29.1"))
 ;; Keywords: convenience, text, wp
 ;; URL: https://github.com/yardquit/garamond
